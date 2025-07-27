e4be7019ed335af7ea8396509ecf2ed6cdc2d1ea
shivam-0718 <shivam.vyas.1807@gmail.com>
2025-07-27 19:44:31 +0530

    Created my Portfolio with Maven and required dependencies.

---
+++ src/test/java/in/shivamvyas/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package in.shivamvyas.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
